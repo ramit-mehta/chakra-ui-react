e1fae40846752f7ad33e69b8fc0f075a4e3fe1db
Ramit Mehta <mail.ramit2499@gmail.com>
2023-02-05 22:08:43 +0530

    videos section done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home';
+import Videos from './components/Videos';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/videos" element={<Videos />} />
       </Routes>
       <Footer />
     </Router>
