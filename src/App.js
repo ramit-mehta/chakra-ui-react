0624f70b13fb2d652096531ac2a1baa964409a50
Ramit Mehta <mail.ramit2499@gmail.com>
2023-02-05 20:57:01 +0530

    footer done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home';
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,85 @@
+import {
+  Box,
+  Button,
+  Heading,
+  HStack,
+  Stack,
+  VStack,
+  Input,
+  Text,
+} from '@chakra-ui/react';
+import React from 'react';
+import { AiOutlineSend } from 'react-icons/ai';
+import { AiOutlineLinkedin } from 'react-icons/ai';
+import { AiOutlineGithub } from 'react-icons/ai';
+import { AiOutlineInstagram } from 'react-icons/ai';
+
+const Footer = () => {
+  return (
+    <Box bgColor={'blackAlpha.900'} minH="40" p={'16'} color="white">
+      <Stack direction={['column', 'row']}>
+        <VStack w={'full'}>
+          <Heading size={'md'} textTransform="uppercase">
+            Subscribe to get Updates
+          </Heading>
+          <HStack>
+            <Input
+              placeholder="Enter your email"
+              borderRadius={'none'}
+              border={'none'}
+              focusBorderColor={'none'}
+              outline="none"
+            />
+            <Button
+              p={'0'}
+              colorScheme="purple"
+              variant={'ghost'}
+              borderRadius={'0 20px 20px 0'}
+            >
+              <AiOutlineSend size={'20'} />
+            </Button>
+          </HStack>
+        </VStack>
+
+        <VStack
+          w={'full'}
+          borderLeft={['none', '1px solid white']}
+          borderRight={['none', '1px solid white']}
+        >
+          <Heading size={'md'} textAlign={'center'} textTransform="uppercase">
+            Video Hub
+          </Heading>
+          <Text color={'whiteAlpha.500'}>All rights reserved.</Text>
+        </VStack>
+
+        <VStack w={'full'}>
+          <Heading size={'md'} textTransform="uppercase">
+            Social Media
+          </Heading>
+          <HStack>
+            <Button colorScheme="purple" variant={'link'}>
+              <a
+                href="https://www.linkedin.com/in/ramit-mehta/"
+                target={'blank'}
+              >
+                <AiOutlineLinkedin size={'25'} />
+              </a>
+            </Button>
+            <Button colorScheme="purple" variant={'link'}>
+              <a href="https://github.com/ramit-mehta" target={'blank'}>
+                <AiOutlineGithub size={'25'} />
+              </a>
+            </Button>
+            <Button colorScheme="purple" variant={'link'}>
+              <a href="https://www.instagram.com" target={'blank'}>
+                <AiOutlineInstagram size={'25'} />
+              </a>
+            </Button>
+          </HStack>
+        </VStack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
