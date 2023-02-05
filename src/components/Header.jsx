c57d7689d54b29b759236ac86f4faf15f1ea5bc8
Ramit Mehta <mail.ramit2499@gmail.com>
2023-02-05 14:24:42 +0530

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,16 +8,92 @@ import {
   DrawerContent,
   DrawerCloseButton,
   Button,
+  useDisclosure,
+  VStack,
+  HStack,
+  Center,
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import { BiMenuAltLeft } from 'react-icons/bi';
 
 const Header = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
-      <Button>
+      <Button
+        pos={'fixed'}
+        left={'4'}
+        top={'4'}
+        colorScheme={'purple'}
+        h={'10'}
+        w={'10'}
+        p={'0'}
+        borderRadius={'full'}
+        onClick={onOpen}
+      >
         <BiMenuAltLeft size={20} />
       </Button>
+
+      <Drawer isOpen={isOpen} placement={'left'} onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton marginTop={2} />
+
+          <DrawerHeader>VIDEO HUB</DrawerHeader>
+
+          <DrawerBody>
+            <VStack>
+              <Button
+                onClick={onClose}
+                variant={'ghost'}
+                colorScheme={'purple'}
+              >
+                <Link to={'/'}>Home</Link>
+              </Button>
+              <Button
+                onClick={onClose}
+                variant={'ghost'}
+                colorScheme={'purple'}
+              >
+                <Link to={'/videos'}>Video</Link>
+              </Button>
+              <Button
+                onClick={onClose}
+                variant={'ghost'}
+                colorScheme={'purple'}
+              >
+                <Link to={'/videos?category=free'}>Free Videos</Link>
+              </Button>
+              <Button
+                onClick={onClose}
+                variant={'ghost'}
+                colorScheme={'purple'}
+              >
+                <Link to={'/upload'}>Upload Video</Link>
+              </Button>
+            </VStack>
+
+            <HStack
+              position={'absolute'}
+              bottom={'10'}
+              left={'50%'}
+              transform={'auto'}
+              translateX={'-50%'}
+            >
+              <Button onClick={onClose} colorScheme={'purple'}>
+                <Link to={'/login'}> Log In</Link>
+              </Button>
+              <Button
+                onClick={onClose}
+                variant={'outline'}
+                colorScheme={'purple'}
+              >
+                <Link to={'/signup'}>Sign Up</Link>
+              </Button>
+            </HStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </>
   );
 };
