fcd0fb9f5d446c6c3dcd75eaa3c73b71c7aa528f
Ramit Mehta <mail.ramit2499@gmail.com>
2023-02-05 18:40:57 +0530

    Home section in progress

---
+++ src/components/Home.jsx
@@ -0,0 +1,36 @@
+import { Box, Heading, Image } from '@chakra-ui/react';
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import img1 from '../assets/1.jpg';
+import img2 from '../assets/2.jpg';
+import img3 from '../assets/3.jpg';
+import img4 from '../assets/4.jpg';
+import img5 from '../assets/5.png';
+
+const Home = () => {
+  return (
+    <Box>
+      <MyCarousel />
+    </Box>
+  );
+};
+const MyCarousel = () => (
+  <Carousel
+    autoPlay
+    infiniteLoop
+    interval={1000}
+    showStatus={false}
+    showThumbs={false}
+    showArrows={false}
+  >
+    <Box w="full" h={'100vh'}>
+      <Image src={img1} />
+      <Heading bgColor={'blackAlpha.600'} color={'white'}>
+        Watch The Future
+      </Heading>
+    </Box>
+  </Carousel>
+);
+
+export default Home;
