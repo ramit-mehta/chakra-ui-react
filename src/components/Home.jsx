a5efdf0a7ea2960659565e56a49fd3a45f87633b
Ramit Mehta <mail.ramit2499@gmail.com>
2023-02-05 19:04:30 +0530

    Header done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Image } from '@chakra-ui/react';
+import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
@@ -8,13 +8,67 @@ import img3 from '../assets/3.jpg';
 import img4 from '../assets/4.jpg';
 import img5 from '../assets/5.png';
 
+// CSS Object
+const headingOptions = {
+  pos: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50%,-50%)',
+  textTransform: 'uppercase',
+  p: '4',
+  size: '2xl',
+};
+
 const Home = () => {
   return (
     <Box>
       <MyCarousel />
+
+      <Container maxW={'container.xl'} minH={'100vh'} p="16">
+        <Heading
+          textTransform={'uppercase'}
+          py="2"
+          w={'fit-content'}
+          borderBottom="2px solid"
+          m={'auto'}
+        >
+          Services
+        </Heading>
+
+        <Stack
+          h={'full'}
+          p={'4'}
+          alignItems="center"
+          direction={['column', 'row']}
+        >
+          <Image src={img5} h={['40', '400']} filter={'hue-rotate(-130deg)'} />
+
+          <Text
+            letterSpacing={'widest'}
+            lineHeight={'190%'}
+            p={['4', '16']}
+            textAlign="center"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat
+            laborum impedit minima officia porro earum nisi deleniti corporis.
+            Quibusdam voluptatibus voluptas, reiciendis eos aspernatur dolore
+            cumque animi dolor soluta eum ut! Delectus maxime nobis atque, nemo
+            rem facere ducimus dicta architecto est cupiditate, magnam pariatur
+            temporibus explicabo sapiente fugit? Sapiente nisi libero aliquam
+            quibusdam, deleniti molestiae soluta tenetur pariatur ratione, sit
+            assumenda explicabo officiis ullam dolorum id dolores voluptate
+            natus debitis voluptatum fuga, harum quas excepturi dolore! Non vel
+            alias cumque recusandae animi blanditiis amet natus ea voluptas sit
+            asperiores repellendus, eius maxime ad illo sint velit ut tempora
+            nam?
+          </Text>
+        </Stack>
+      </Container>
     </Box>
   );
 };
+
+// MyCarousel component
 const MyCarousel = () => (
   <Carousel
     autoPlay
@@ -26,8 +80,28 @@ const MyCarousel = () => (
   >
     <Box w="full" h={'100vh'}>
       <Image src={img1} />
-      <Heading bgColor={'blackAlpha.600'} color={'white'}>
+      <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
         Watch The Future
+      </Heading>
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img2} />
+      <Heading bgColor={'whiteAlpha.400'} color={'black'} {...headingOptions}>
+        Future is Gaming
+      </Heading>
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img3} />
+      <Heading bgColor={'whiteAlpha.400'} color={'black'} {...headingOptions}>
+        Gaming on Console
+      </Heading>
+    </Box>
+    <Box w="full" h={'100vh'}>
+      <Image src={img4} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        The World of Lofi
       </Heading>
     </Box>
   </Carousel>
