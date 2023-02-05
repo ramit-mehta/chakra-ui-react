24ed5290baa64bc0e7620df23fc796aa89e0e4d8
Ramit Mehta <mail.ramit2499@gmail.com>
2023-02-05 14:32:43 +0530

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {
   Drawer,
   DrawerBody,
-  DrawerFooter,
   DrawerHeader,
   DrawerOverlay,
   DrawerContent,
@@ -11,7 +10,6 @@ import {
   useDisclosure,
   VStack,
   HStack,
-  Center,
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import { BiMenuAltLeft } from 'react-icons/bi';
