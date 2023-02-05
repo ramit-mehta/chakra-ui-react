879f12df01471af154c57b3968ac6eb7bef1955e
Ramit Mehta <mail.ramit2499@gmail.com>
2023-02-05 13:40:39 +0530

    header in progress

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Button,
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { BiMenuAltLeft } from 'react-icons/bi';
+
+const Header = () => {
+  return (
+    <>
+      <Button>
+        <BiMenuAltLeft size={20} />
+      </Button>
+    </>
+  );
+};
+export default Header;
