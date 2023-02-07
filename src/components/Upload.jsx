41d20d8fa2b2b8110f4cb4a1e9fdb295fa2b0a0e
Ramit Mehta <mail.ramit2499@gmail.com>
2023-02-07 21:15:51 +0530

    free video done and upload section in progress

---
+++ src/components/Upload.jsx
@@ -0,0 +1,8 @@
+import { Container } from '@chakra-ui/react';
+import React from 'react';
+
+const Upload = () => {
+  return <Container maxW={'container.xl'} h={'100vh'} p="16"></Container>;
+};
+
+export default Upload;
