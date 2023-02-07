f5002dd337b453913569308a6a3786532d8e26fb
Ramit Mehta <mail.ramit2499@gmail.com>
2023-02-07 21:24:58 +0530

    upload section done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home';
+import Upload from './components/Upload';
 import Videos from './components/Videos';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/videos" element={<Videos />} />
+        <Route exact path="/upload" element={<Upload />} />
       </Routes>
       <Footer />
     </Router>
