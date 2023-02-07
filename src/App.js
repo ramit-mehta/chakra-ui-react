4dad2bd4b6229460ec36dff6b007acef395331e0
Ramit Mehta <mail.ramit2499@gmail.com>
2023-02-07 21:44:06 +0530

    login page done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home';
+import { Login } from './components/Login';
+import SignUp from './components/SignUp';
 import Upload from './components/Upload';
 import Videos from './components/Videos';
 
@@ -13,6 +15,8 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/videos" element={<Videos />} />
         <Route exact path="/upload" element={<Upload />} />
+        <Route exact path="/signup" element={<SignUp />} />
+        <Route exact path="/login" element={<Login />} />
       </Routes>
       <Footer />
     </Router>
